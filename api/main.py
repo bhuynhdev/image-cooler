b22fd68422ea36cb1634c3906642146609f84876
bhuynhdev <shinsusenju@aol.com>
2022-04-28 22:40:13 -0400

    API: Add CORS middleware

--- api/main.py
+++ api/main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
     return "Server is running"
+
+
+@app.post("/upload")
+async def uploadImage(image: UploadFile):
+    return {"file": image.filename}
