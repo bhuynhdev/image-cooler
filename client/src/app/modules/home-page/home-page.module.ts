1a61d82ac3773e7485bdb1298371f572806e85d5
bhuynhdev <shinsusenju@aol.com>
2022-05-03 15:35:28 -0400

    Client: Improve layout using Nebular layout components

--- client/src/app/modules/home-page/home-page.module.ts
+++ client/src/app/modules/home-page/home-page.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
-import { NbButtonModule, NbInputModule } from '@nebular/theme';
+import { NbButtonModule, NbInputModule, NbLayoutModule } from '@nebular/theme';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, NbInputModule, NbButtonModule],
+  imports: [CommonModule, NbInputModule, NbButtonModule, NbLayoutModule],
   exports: [HomeComponent],
 })
 export class HomePageModule {}
