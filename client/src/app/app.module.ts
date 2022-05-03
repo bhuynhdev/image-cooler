415453fd2a6c7eb32f5f0ae7855a2541c74c02d0
bhuynhdev <shinsusenju@aol.com>
2022-05-03 11:58:37 -0400

    Client: Add Nebular UI

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,10 +6,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ImageService } from './services/image.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    AppRoutingModule,
+  ],
   providers: [ImageService],
   bootstrap: [AppComponent],
 })
