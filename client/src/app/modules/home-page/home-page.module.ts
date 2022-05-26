e71613cc6037db395a38243808fba79e5a441a38
bhuynhdev <shinsusenju@aol.com>
2022-05-26 18:09:35 -0400

    Client: Design Hero section with Angular Material

--- client/src/app/modules/home-page/home-page.module.ts
+++ client/src/app/modules/home-page/home-page.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home.component';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatButtonModule],
   exports: [HomePageComponent],
 })
 export class HomePageModule {}
