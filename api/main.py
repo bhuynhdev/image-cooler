272e2b5eb87fe05667937e947c58c9ef1d73adab
bhuynhdev <shinsusenju@aol.com>
2022-06-07 17:51:02 -0400

    API: Accept dynamic src_type

--- api/main.py
+++ api/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
-from photomosaics import create_photomosaics
-from fastapi.responses import FileResponse
 from io import BytesIO
+
+from fastapi import FastAPI, Form, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+
+from photomosaics import create_photomosaics
 
 app = FastAPI()
 
@@ -20,9 +22,17 @@ async def root():
     return "Server is running"
 
 
+source_type_to_folder_name = {
+    "cat": "cat999",
+    "dog": "dog1000",
+    "anime": "danborou-anime-0007"
+}
+
+
 @app.post("/upload")
-async def uploadImage(image: UploadFile):
-    source_folder = "./SourceImages/danborou-anime-0007"
+async def uploadImage(image: UploadFile, src_type: str = Form(default="cat")):
+    print(f"Received {image.filename} with {src_type=}")
+    source_folder = "./SourceImages/" + source_type_to_folder_name[src_type]
     request_object_content = await image.read()
     output_path = create_photomosaics(BytesIO(request_object_content), source_folder)
     return FileResponse(output_path)
