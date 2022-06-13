8e5abeebe866bf937b1c7bf6f5eda872f895606a
bhuynhdev <shinsusenju@aol.com>
2022-06-12 22:49:05 -0400

    Client: Change project name and update dependencies

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'photomosaic-web-client'`, () => {
+  it(`should have as title 'image-cooler-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('photomosaic-web-client');
+    expect(app.title).toEqual('image-cooler-client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('photomosaic-web-client app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'image-cooler-client app is running!'
+    );
   });
 });
